c9f7c46b70490a948d9b2beb3b06112a149e1c35
DJ Daugherty <ddaugher@gmail.com>
2014-06-14 08:34:10 -0400

    semi minor

--- src/test/groovy/com/studio945/geodesy/EllipsoidTests.groovy
+++ src/test/groovy/com/studio945/geodesy/EllipsoidTests.groovy
@@ -88,7 +88,7 @@ public class EllipsoidTests extends spock.lang.Specification {
 
     then:
     6378135.0 == e.semiMajorAxis
-    6356774.719195305 == e.semiMinorAxis
+    6356750.520016094 == e.semiMinorAxis
     0.003352891869237217 == e.flattening
     298.25 == e.inverseFlattening
   }
