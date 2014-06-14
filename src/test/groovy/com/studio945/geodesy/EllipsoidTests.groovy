ed957c91e8d2879bf2bbf938dff14eb42f7c2642
DJ Daugherty <ddaugher@gmail.com>
2014-06-14 08:19:05 -0400

    adding tests for Ellipsoid

--- src/test/groovy/com/studio945/geodesy/EllipsoidTests.groovy
+++ src/test/groovy/com/studio945/geodesy/EllipsoidTests.groovy
@@ -17,4 +17,35 @@ public class EllipsoidTests extends spock.lang.Specification {
     0.0d == e.flattening
     -1.0d == e.inverseFlattening
   }
+
+  def "when creating Ellipsoid from semiMajorAxis and flattening, should return original flattening value"() {
+    when: "create new ellipsoid"
+    def semiMajorAxis = 100.0d
+    def flattening = 1000.0d
+    def e = Ellipsoid.fromAAndF(semiMajorAxis, flattening)
+
+    then:
+    1000.0d == e.flattening
+  }
+
+  def "when creating Ellipsoid from semiMajorAxis and inverseFlattening, should return original flattening value"() {
+    when: "create new ellipsoid"
+    def semiMajorAxis = 100.0d
+    def inverseFlattening = 1000.0d
+    def e = Ellipsoid.fromAAndF(semiMajorAxis, inverseFlattening)
+
+    then:
+    0.0010d == e.inverseFlattening
+  }
+
+  def "should properly create WGS84 ellipsoid"() {
+    when: "create WGS84 ellipsoid"
+    def e = Ellipsoid.WGS84
+
+    then:
+    6378137.0 == e.semiMajorAxis
+    6356752.314245179 == e.semiMinorAxis
+    0.0033528106647474805 == e.flattening
+    298.257223563 == e.inverseFlattening
+  }
 }
