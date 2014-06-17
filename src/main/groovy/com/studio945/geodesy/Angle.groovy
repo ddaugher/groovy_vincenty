eb87ce1a4eb5509399b5802921dfa625e9c2cfb8
DJ Daugherty <ddaugher@gmail.com>
2014-06-16 22:41:55 -0400

    refactor/cleanup

--- src/main/groovy/com/studio945/geodesy/Angle.groovy
+++ src/main/groovy/com/studio945/geodesy/Angle.groovy
@@ -5,13 +5,7 @@ class Angle
 {
    def static final double piOver180 = Math.PI / 180.0;
    
-   def static double toRadians( double degrees )
-   {
-      return degrees * piOver180
-   }
+   def static double toRadians( double degrees ) { degrees * piOver180 }
    
-   def static double toDegrees( double radians )
-   {
-      return radians / piOver180
-   }
+   def static double toDegrees( double radians ) { radians / piOver180 }
 }
